961d81f23bd8647cc50529e636dcc419b7bc8962
s31261 <s31261@pjwstk.edu.pl>
2025-03-13 13:32:25 +0100

    property and full property

--- Wyklad1/Program.cs
+++ Wyklad1/Program.cs
@@ -7,3 +7,16 @@ class Program
         Console.WriteLine("Hello, World!");
     }
 }
+
+class Student
+{
+    public string Name { get; set; }
+    private int _age;
+
+    public int Age
+    {
+        get { return _age; }
+        set { _age = value; }
+    }
+    
+}
